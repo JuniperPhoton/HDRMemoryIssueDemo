b6c2a228f25181ed176b0f0b9d579e78414b335c
juniperphoton <dengweichao@hotmail.com>
2024-01-27 23:16:30 +0800

    Initial Commit

---
+++ HDRMemoryIssueDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HDRMemoryIssueDemo
+//
+//  Created by Photon Juniper on 2024/1/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HDRMemoryIssueDemo/HDRMemoryIssueDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDRMemoryIssueDemoApp.swift
+//  HDRMemoryIssueDemo
+//
+//  Created by Photon Juniper on 2024/1/27.
+//
+
+import SwiftUI
+
+@main
+struct HDRMemoryIssueDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
